ba2c1b1fdcade629f69c151978a2639e9122f012
eshk12 <eshk12@users.noreply.github.com>
2022-05-01 01:24:34 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-nc mysql_db:3306
-
-java" "-jar" "project.jar
+state=0
+while [ $state -eq 0 ]; do
+  nc -z -v mysql_container 3306;
+  if [ $? -eq 0 ];then
+    state=1;
+  else
+    echo "Wait for mysql"
+    sleep 3;
+  fi;
+  done;
+  
+java -jar project.jar
